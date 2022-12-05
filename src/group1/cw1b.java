d1f0658ea77d7eeb46e6565d749b8af4bff8fdac
Oskar2706 <alexvaraksa@bk.ru>
2022-12-05 21:18:42 +0300

    dz1

---
+++ src/group1/cw1b.java
@@ -0,0 +1,42 @@
+package group1;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+public class cw1b {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("какого размера массив вам нужен");
+        int a = scanner.nextInt();
+        Random random = new Random();
+        int x = 0;
+        int i, j;
+        int[] array = new int[a];
+        for (i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(30);
+        }
+        System.out.println(Arrays.toString(array));
+        a = scanner.nextInt();
+        for (int d : array) {
+            if (d == a) {
+                x += 1;
+            }
+        }
+        if (x != 0) {
+            System.out.println("удален елемент " +a);
+            int[] array2 = new int[array.length - x];
+            for (i = 0, j = 0; i < array.length; i++) {
+                if (array[i] == a) {
+                } else {
+                    array2[j] = array[i];
+                    j++;
+                }
+            }
+            System.out.println(Arrays.toString(array2));
+        } else {
+            System.out.println("введите дргое число" + Arrays.toString(array));
+
+        }
+    }
+}
---
+++ src/group1/cw4.java
@@ -0,0 +1,7 @@
+package group1;
+
+public class cw4 {
+    public static void main(String[] args) {
+
+    }
+}
