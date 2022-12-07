4fa55ae2626cbfebc8ac25b3684ce4d4b76a388e
Oskar2706 <alexvaraksa@bk.ru>
2022-12-07 16:45:36 +0300

    dz3

---
+++ src/group2/cw3.java
@@ -0,0 +1,11 @@
+package group2;
+
+import java.util.Arrays;
+
+public class cw3 {
+    public static void main(String[] args) {
+        String[] names = {"Evgeniya","Alexander","Myska","Ksyshka"};
+        Arrays.sort(names);
+        System.out.println(Arrays.toString(names));
+    }
+}
