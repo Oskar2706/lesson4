7fdcff420fe7454fe2d11a966c42917fd1e22890
Oskar2706 <alexvaraksa@bk.ru>
2022-12-07 16:33:56 +0300

    dz2

---
+++ src/group2/cw2.java
@@ -0,0 +1,24 @@
+package group2;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class cw2 {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int[] array = new int[15];
+        int i;
+        for (i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(10);
+        }
+        System.out.print(Arrays.toString(array));
+        System.out.println(" ");
+        for (i = 0; i < array.length; i++) {
+            if (i % 2 == 1) {
+                array[i] = 0;
+            }
+        }
+        System.out.print(Arrays.toString(array));
+    }
+    }
+
